9e7bedbbec9b1d217eccac2911c53b55855a5154
shamim-anowar <shamim.hire@gmail.com>
2025-03-14 09:47:00 +0600

    complete the rest code

--- module-8.py
+++ module-8.py
@@ -9,3 +9,42 @@ def next_smaller_elements(n, arr):
         stack.append(i)
 
     return result
+
+
+class QueueUsingTwoStacks:
+    def __init__(self):
+        self.push_stack = []
+        self.pop_stack = []
+
+    def enqueus(self, item):
+        self.push_stack.append(item)
+        
+    def dequeue(self):
+        if len(self.pop_stack) == 0:
+            self._shift_push_to_pop_stack()
+        
+        if len(self.pop_stack) == 0:
+            return -1
+        
+        value = self.pop_stack.pop()
+        print(value)
+        return value
+    
+    def front(self):
+        if len(self.pop_stack) == 0:
+            self._shift_push_to_pop_stack()
+        
+        if len(self.pop_stack) == 0:
+            return -1
+        
+        value = self.pop_stack[-1]
+        print(value)
+        return value
+    
+    def _shift_push_to_pop_stack(self):
+        while len(self.push_stack) != 0:
+            self.pop_stack.append(self.push_stack.pop())
+
+    def get_data(self):
+        print(f"Push stack = {self.push_stack} and Pop stack = {self.pop_stack}")
+        return self._shift_push_to_pop_stack()
